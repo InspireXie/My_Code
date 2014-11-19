1eefc76865014378d50fcb6e5d80e623be6ac66f
InspireXie <inspirex@126.com>
2014-11-19 21:12:39 +0800

    Create encoding.java

---
+++ encoding.java
@@ -0,0 +1,20 @@
+//获取系统默认编码
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+
+class encoding{
+	public static void main(String[] args) {
+        System.out.println("Default Charset=" + Charset.defaultCharset());
+        System.out.println("file.encoding=" + System.getProperty("file.encoding"));
+        System.out.println("Default Charset=" + Charset.defaultCharset());
+        System.out.println("Default Charset in Use=" + getDefaultCharSet());
+    }
+
+    private static String getDefaultCharSet() {
+        OutputStreamWriter writer = new OutputStreamWriter(new ByteArrayOutputStream());
+        String enc = writer.getEncoding();
+        return enc;
+    }
+
+}
